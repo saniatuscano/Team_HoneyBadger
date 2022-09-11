7e062a94fd020fd995bf30efd203adfbaab81d7a
Sakshi Shetty <sakshishetty1010@gmail.com>
2022-09-11 20:01:30 +0530

    commit

--- src/layouts/authentication/sign-up/ngo.js
+++ src/layouts/authentication/sign-up/ngo.js
@@ -44,6 +44,9 @@ const API_KEY = 'TdG0aL81ZyzIuoW057KWfQROSdT8giBJHLZTkCnpvE0'
 
 import {InputLabel, MenuItem, Select } from '@mui/material';
 import Navbar from "layouts/home/components/Navbar";
+window.Buffer = window.Buffer || require('buffer').Buffer;
+import {cli as client} from '../../../ipfs-config'
+import {Disaster, web3} from '../../../Disaster'
 
 function NGO() {
   const [address,setAddress] = useState('')
@@ -52,34 +55,55 @@ function NGO() {
   const [email,setEmail] = useState('')
   const [phone,setPhone] = useState('')
   const [password,setPassword] = useState('')
-  const [lat,setLat] = useState('')
-  const [lon, setLon] = useState('')
   const navigate = useNavigate()
+ const [file,setFile] = useState('')
 
+  const captureFile = (e) => {
+    const data = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      setFile(Buffer(reader.result));
+    }
+
+
+    e.preventDefault(); 
+  }
 
   const onSubmit = async(e) => {
     e.preventDefault();
-    const newObj = {
-      name, email,phoneno : phone, password
+    try {
+      const created = await client.add(file);
+      hash = created.path
+        let accounts = await web3.eth.getAccounts();
+        console.log("**********8",hash)
+        let registerOrg = await Disaster.methods.addOrg(name,email,password,phone,location,hash).send({from : accounts[0]})
+        navigate('/sign-in/ngo')
+     
+    } catch (error) {
+      console.log(error.message)
     }
-    const res = await axios.post('http://localhost:7000/registerNgo',newObj);
-    if(res.data.status){
-      navigate('/sign-in/ngo');
-    }
-    else console.log("Falied to register")
+    // const newObj = {
+    //   name, email,phoneno : phone, password
+    // }
+    // const res = await axios.post('http://localhost:7000/registerNgo',newObj);
+    // if(res.data.status){
+    //   navigate('/sign-in/ngo');
+    // }
+    // else console.log("Falied to register")
   }
 
-  useEffect(()=> {
-    const here_api = async () => {
-    if(address.length>0){
-      const res = await axios.get(`https://geocode.search.hereapi.com/v1/geocode?q=${address}&apiKey=${API_KEY}`)
-    setResponse(res.data.items)
-    console.log(response)
+//   useEffect(()=> {
+//     const here_api = async () => {
+//     if(address.length>0){
+//       const res = await axios.get(`https://geocode.search.hereapi.com/v1/geocode?q=${address}&apiKey=${API_KEY}`)
+//     setResponse(res.data.items)
+//     console.log(response)
 
-  }
-    }
-    here_api()
-  },[address])
+//   }
+//     }
+//     here_api()
+//   },[address])
   
 
   return (
@@ -115,7 +139,7 @@ function NGO() {
               value = {name}
               label="Name" variant="standard" fullWidth />
             </MDBox>
-
+</MDBox>
             <MDBox mb={2}>
               <MDInput
                type="email" 
@@ -147,40 +171,23 @@ function NGO() {
 
             <MDBox mb={2}>
 
-              <MDInput type="file" label="Upload file" variant="standard" fullWidth /*onChange={captureFile}*/ />
-
-              <MDInput type="file" label="Upload file" variant="standard" fullWidth  />
+              <MDInput type="file" label="Upload file" variant="standard" fullWidth onChange={captureFile} />
 
             </MDBox>
           
           
   
-            <MDBox mb={2}>
-            {/* <label >Select 
-            <select >
-            {response && 
-               response.map ( (r,i)=> {
-              <option value={i}>{r.title}</option>
-               })
-              }
-            </select></label> */}
-
-              <MDInput type="text" label="Location" variant="standard" onChange={e => setAddress(e.target.value)} fullWidth />
-             
-    
-           <Select label="Select location from the dropdown">
-           {response.map(r => 
-              <MenuItem 
-              onClick={e=> {
-                setLat(r.position.lat);
-                setLon(r.position.lng)
-                setAddress(r.address.label)
-              }}>{r.address.label}</MenuItem>)
-            }
+            <MDBox pt={4} pb={3} px={3}>
          
-          </Select>
-  
+            <MDBox mb={2}>
+              <MDInput 
+              type="text" 
+              name = "address"
+              onChange = { e => setAddress(e.target.value)}
+              value = {address}
+              label="Location" variant="standard" fullWidth />
             </MDBox>
+
             <MDBox display="flex" alignItems="center" ml={-1}>
               <Checkbox />
               <MDTypography
